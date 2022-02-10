374423eb838bfce04ac950c3732dd5daa2406d8a
jamirolol <2048415@talnet.nl>
2022-02-10 15:58:25 +0100

    ep4

---
+++ 2-Loops/Opdracht-2.2/for-loops.php
@@ -0,0 +1,10 @@
+<?php
+$x=0;
+
+for($x = 0 ; $x < 18;){
+	echo "Ik ben " . $x . " jaar, dus ik mag nog niet stemmen";
+	echo "<br>";
+	$x++;
+
+}
+echo "ik ben " . $x . " ik mag eindelijk stemmen";
