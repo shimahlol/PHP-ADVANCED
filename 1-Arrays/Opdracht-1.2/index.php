745f7c869d74f4bb188583af1c336462dca1b74f
jamirolol <2048415@talnet.nl>
2022-02-10 14:48:57 +0100

    ep2

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,6 @@
+<?php
+$student ['voornaam'] = "Willem ";
+$student ['achternaam'] = "Alexander van Oranje ";
+$student ['leeftijd'] = 53;
+$student ['opleiding'] = 'Software Ontwikkeling ';
+echo $student['voornaam'] . $student['achternaam'] . "is " . $student['leeftijd'] . " en is student aan het ROC van Amsterdam aan de opleiding ". $student['opleiding'];
