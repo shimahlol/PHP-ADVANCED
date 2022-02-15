cc65a57119c7f9b2206d033785889780d0344a8b
jamirolol <2048415@talnet.nl>
2022-02-15 10:26:28 +0100

    git

---
+++ 2-Loops/Opdracht-2.3/index.php
@@ -0,0 +1,9 @@
+<?php
+$dagen = array("maandag", "dinsadg", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+foreach($dagen as $kleur){
+	echo "<ul>
+	<li>
+	" . $kleur . "
+	</li>
+	</ul>";
+}
