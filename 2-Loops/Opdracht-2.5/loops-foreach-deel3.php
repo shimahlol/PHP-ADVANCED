7f9a7966ee368387d64cda31a94406979927a62e
jamirolol <2048415@talnet.nl>
2022-02-27 23:20:26 +0100

    gitfixed?

---
+++ 2-Loops/Opdracht-2.5/loops-foreach-deel3.php
@@ -0,0 +1,16 @@
+<?php
+
+$student  = array(
+	"voornaam"    => "Jan",
+	"achternaam"  => "Janssen",
+	"klas"        => "9A",
+	"Leeftijd"    => 17,
+	"Woonplaats"  => "Amstelveen"
+  );
+  
+  // );
+  // I am very sad :(
+foreach($student as $keys => $value){
+	echo"keys => $value";
+}
+?>
