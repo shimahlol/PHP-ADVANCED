e258bd6aed2d13aed94abd9491235eebd970ea1a
jamirolol <2048415@talnet.nl>
2022-03-04 11:53:48 +0100

    functie2

---
+++ 4-Functions/Opdracht-4.2/functies2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<?php
+function rekenuit(int $a, int $b, int $c){
+	$d = $a + $b + $c ;
+	echo "Als ik de getallen $a, $b en $c bij elkaar optel, is de uitkomst $d";
+}
+rekenuit(1, 2, 4)
+?>
+</body>
+</html>
